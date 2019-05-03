1eabac0f1c97266356f33e115567903869a24e48
Zeboto <zebrawii@gmail.com>
2019-05-03 17:48:04 -0400

    Added blurplefy command

--- bot/cogs/blurple/cog.py
+++ bot/cogs/blurple/cog.py
@@ -26,7 +26,18 @@ def _make_color_command(name, modifier, **kwargs):
             else:
                 url = who.avatar_url
 
-        data = {'modifier': modifier, 'method': method, 'variation': variations, 'url': str(url), 'channel': ctx.channel.id, 'requester': ctx.author.id, 'message': ctx.message.id}
+        if modifier is 'blurplefy':
+            if ctx.guild.get_role(self.bot.config['blurple_light_id']) in ctx.author.roles:
+                final_modifier = 'light'
+            elif ctx.guild.get_role(self.bot.config['blurple_dark_id']) in ctx.author.roles:
+                final_modifier = 'dark'
+            else:
+                await ctx.channel.send('You need to be a part of a team first.')
+                return
+        else:
+            final_modifier = modifier
+
+        data = {'modifier': final_modifier, 'method': method, 'variation': variations, 'url': str(url), 'channel': ctx.channel.id, 'requester': ctx.author.id, 'message': ctx.message.id}
 
         # Signal that the request has been queued
         await ctx.message.add_reaction(self.bot.config['queue_emoji'])
@@ -41,3 +52,4 @@ def _make_color_command(name, modifier, **kwargs):
 class Blurplefy(Cog):
     lightfy = _make_color_command('lightfy', 'light')
     darkfy = _make_color_command('darkfy', 'dark')
+    blurplefy = _make_color_command('blurplefy', 'blurplefy')
