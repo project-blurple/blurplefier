939afbc9899bd223b4fb37beff25bfe71e3b5dcb
Zeboto <zebrawii@gmail.com>
2021-05-08 10:38:53 -0400

    Remove debug logs

--- app.py
+++ app.py
@@ -174,17 +174,17 @@ def inner_handler(event, context):
             send_response(interaction, data=data, followup=True)
             return
 
-        # if int(resp.headers.get('content-length', '0')) > 1024 ** 2 * 8:
-        #     data = {'content': f'Your image is too large (> 8MiB) <@!{user_id}>! {SAD_EMOJI}'}
-        #     send_response(interaction, data=data, followup=True)
-        #     return
+        if int(resp.headers.get('content-length', '0')) > 1024 ** 2 * 8:
+            data = {'content': f'Your image is too large (> 8MiB) <@!{user_id}>! {SAD_EMOJI}'}
+            send_response(interaction, data=data, followup=True)
+            return
 
-        # try:
-        data = {'content': f'Your requested image is ready <@!{user_id}>!'}
-        image = Image(*magic.convert_image(resp.content, 'light', method, variations))
-        # except Exception:
-        #     image = None
-        #     data = {'content': f'I was unable to blurplefy your image <@!{user_id}>! {SAD_EMOJI}'}
+        try:
+            data = {'content': f'Your requested image is ready <@!{user_id}>!'}
+            image = Image(*magic.convert_image(resp.content, 'light', method, variations))
+        except Exception:
+            image = None
+            data = {'content': f'I was unable to blurplefy your image <@!{user_id}>! {SAD_EMOJI}\nPlease try a different image.'}
         resp = send_response(interaction, data=data, image=image, followup=True)
 
         if 400 <= resp.status_code <= 599:
